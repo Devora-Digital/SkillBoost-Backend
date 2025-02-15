dff80b38491a1ae22f8d9616e80ce6ecd56ddfc1
Annyni <raindbowcat2022@gmail.com>
2025-02-15 19:42:09 +0400

    init project skeleton

---
+++ src/test/java/com/skillboost/SkillBoostBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillBoostBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
